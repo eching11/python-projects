b2c5fdff3df1637fc46d0358f4ec0ffdf59cf803
Erica Ching <36242984+eching11@users.noreply.github.com>
2021-01-04 23:05:30 -0800

    added scan options for nmap scanner

--- nmap-scanner.py
+++ nmap-scanner.py
@@ -23,3 +23,27 @@ response = input("""Which type of scan?
                     4. Exit""")
 
 print("You have selected option: ", response)
+
+if response == '1':
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports: ", scanner[ip_addr]['tcp'].keys())
+elif response == '2':
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports: ", scanner[ip_addr]['udp'].keys())
+elif response == '3':
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports: ", scanner[ip_addr]['tcp'].keys())
+else
+    print("Please enter a valid option.")
